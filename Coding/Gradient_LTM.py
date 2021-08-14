c9161ca6e36951d8ddb5e5e484df6195fcce35bc
jbeckerl <86257745+jbeckerl@users.noreply.github.com>
2021-08-14 13:48:42 -0400

    Update Gradient_LTM.py

--- Coding/Gradient_LTM.py
+++ Coding/Gradient_LTM.py
@@ -23,7 +23,7 @@ for a in [x for x in range(4,17) if x != 10]:
 #sns.heatmap(C)
 
 #Thresholding
-    threshold = .99 #bottom X percent of edge weights are eliminated
+    threshold = .4 #bottom X percent of edge weights are eliminated
     C_thresh = copy.deepcopy(C)
     number_positive_edges = len(np.where(C > 0)[0])
     cutoff = int(np.round(number_positive_edges*threshold))
@@ -34,6 +34,7 @@ for a in [x for x in range(4,17) if x != 10]:
 
 #Cascading
 #parameters
+    threshold=.02
     seed = 36 #ROI number where activity starts
     time_steps = 25 #number of time steps before termination of simulation
     N = len(C_thresh) #number of nodes
